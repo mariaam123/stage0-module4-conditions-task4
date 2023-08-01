d9e1e35989c14a1907644d1dd05f023e705cee4a
mariaam123 <mariam.bakradze.1@btu.edu.ge>
2023-08-02 01:23:40 +0400

    completed tasks

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+
+        if(first >= second && first >= third ){
+            System.out.println(first);
+        }
+        else if (second >= first && second >= third){
+            System.out.println(second);}
+        else if (third >= first && third >= second){
+            System.out.println(third);}
+        {
+        }
     }
-}
+    public static void main(String[] args) {
+        MaxNumberPrinter maxNumberPrinter = new MaxNumberPrinter();
+        int first = 1;
+        int second = 2;
+        int third = 1;
+        maxNumberPrinter.printGreatest(first,second,third);
+    }}
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+
+        int result = (first > second)? 10 : -10;
+        System.out.println(result);
+        }
+
+    public static void main(String[] args) {
+        TernaryAssignment ternaryAssignment = new TernaryAssignment();
+        int first = 5;
+        int second = 2;
+        ternaryAssignment.assignAndPrintBasedOnWhichBigger(first,second);
     }
 }
